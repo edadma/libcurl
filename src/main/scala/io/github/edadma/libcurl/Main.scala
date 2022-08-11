8de155f4be52ef84f7df85cc3f2619a1939bf728
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-10 20:11:51 -0400

    basic

--- src/main/scala/io/github/edadma/libcurl/Main.scala
+++ src/main/scala/io/github/edadma/libcurl/Main.scala
@@ -1,21 +1,81 @@
 package io.github.edadma.libcurl
 
-import scala.scalanative.unsafe._
-import scala.scalanative.libc._
-import LibCurl._, LibCurlConstants._
-import stdlib._
-import stdio._
-import string._
+import scala.scalanative.unsafe.*
+import scala.scalanative.unsigned.*
+import scala.scalanative.libc.*
+import LibCurl.*
+import LibCurlConstants.*
+
+import scala.collection.mutable
+import stdlib.*
+import stdio.*
+import string.*
 import scala.collection.mutable.HashMap
 
-@main def run(): Unit =
-  println("initializing")
-  global_init(1)
-  val resp = getSync(args(0))
-  println(s"done.  got response: $resp")
-  println("global cleanup...")
-  global_cleanup()
-  println("done")
+import scala.scalanative.runtime.Boxes
+import scala.scalanative.runtime.Intrinsics
+
+@main def run(url: String): Unit =
+  var request_serial = 0L
+  val responses = mutable.HashMap[Long, ResponseState]()
+
+  val statusLine = raw".+? (\d+) (.+)\n".r
+  val headerLine = raw"([^:]+): (.*)\n".r
+
+  def func_to_ptr(f: Object): Ptr[Byte] = {
+    Boxes.boxToPtr[Byte](Boxes.unboxToCFuncPtr4(f))
+  }
+
+  def int_to_ptr(i: Int): Ptr[Byte] = {
+    Boxes.boxToPtr[Byte](Intrinsics.castIntToRawPtr(i))
+  }
+
+  def long_to_ptr(l: Long): Ptr[Byte] = {
+    Boxes.boxToPtr[Byte](Intrinsics.castLongToRawPtr(l))
+  }
+
+  def bufferToString(ptr: Ptr[Byte], size: CSize, nmemb: CSize): String = {
+    val byteSize = size * nmemb
+    val buffer = malloc(byteSize + 1.toUInt)
+    strncpy(buffer, ptr, byteSize + 1.toUInt)
+    val res = fromCString(buffer)
+    free(buffer)
+    res
+  }
+
+  val writeCB: CurlDataCallback =
+    (ptr: Ptr[Byte], size: CSize, nmemb: CSize, data: Ptr[Byte]) => {
+      val serial = !(data.asInstanceOf[Ptr[Long]])
+      val len = stackalloc[Double]()
+      !len = 0
+      val strData = bufferToString(ptr, size, nmemb)
+
+      val resp = responses(serial)
+      resp.body = resp.body + strData
+      responses(serial) = resp
+
+      return size * nmemb
+    }
+
+  val headerCB: CurlDataCallback =
+    (ptr: Ptr[Byte], size: CSize, nmemb: CSize, data: Ptr[Byte]) => {
+      val serial = !(data.asInstanceOf[Ptr[Long]])
+      val len = stackalloc[Double]()
+      !len = 0
+      val byteSize = size * nmemb
+      val headerString = bufferToString(ptr, size, nmemb)
+      headerString match {
+        case statusLine(code, description) =>
+          println(s"status code: $code $description")
+        case headerLine(k, v) =>
+          val resp = responses(serial)
+          resp.headers(k) = v
+          responses(serial) = resp
+        case l =>
+      }
+      fwrite(ptr, size, nmemb, stdout)
+      byteSize
+    }
 
   def addHeaders(curl: Curl, headers: Seq[String]): Ptr[CurlSList] = {
     var slist: Ptr[CurlSList] = null
@@ -30,9 +90,6 @@ import scala.collection.mutable.HashMap
     slist_append(slist, toCString(header))
   }
 
-  var request_serial = 0L
-  val responses = HashMap[Long, ResponseState]()
-
   def getSync(url: String, headers: Seq[String] = Seq.empty): ResponseState = {
     val req_id_ptr = malloc(sizeof[Long]).asInstanceOf[Ptr[Long]]
     !req_id_ptr = 1 + request_serial
@@ -44,58 +101,19 @@ import scala.collection.mutable.HashMap
       val url_str = toCString(url)
       println(curl_easy_setopt(curl, URL, url_str))
     }
-    curl_easy_setopt(curl, WRITECALLBACK, Curl.func_to_ptr(writeCB))
+    curl_easy_setopt(curl, WRITECALLBACK, func_to_ptr(writeCB))
     curl_easy_setopt(curl, WRITEDATA, req_id_ptr.asInstanceOf[Ptr[Byte]])
-    curl_easy_setopt(curl, HEADERCALLBACK, Curl.func_to_ptr(headerCB))
+    curl_easy_setopt(curl, HEADERCALLBACK, func_to_ptr(headerCB))
     curl_easy_setopt(curl, HEADERDATA, req_id_ptr.asInstanceOf[Ptr[Byte]])
     val res = easy_perform(curl)
     easy_cleanup(curl)
-    return responses(request_serial)
+    responses(request_serial)
   }
 
-  def bufferToString(ptr: Ptr[Byte], size: CSize, nmemb: CSize): String = {
-    val byteSize = size * nmemb
-    val buffer = malloc(byteSize + 1)
-    strncpy(buffer, ptr, byteSize + 1)
-    val res = fromCString(buffer)
-    free(buffer)
-    return (res)
-  }
-
-  val writeCB = new CurlDataCallback {
-    def apply(ptr: Ptr[Byte], size: CSize, nmemb: CSize, data: Ptr[Byte]): CSize = {
-      val serial = !(data.asInstanceOf[Ptr[Long]])
-      val len = stackalloc[Double]
-      !len = 0
-      val strData = bufferToString(ptr, size, nmemb)
-
-      val resp = responses(serial)
-      resp.body = resp.body + strData
-      responses(serial) = resp
-
-      return size * nmemb
-    }
-  }
-
-  val statusLine = raw".+? (\d+) (.+)\n".r
-  val headerLine = raw"([^:]+): (.*)\n".r
-  val headerCB = new CurlDataCallback {
-    def apply(ptr: Ptr[Byte], size: CSize, nmemb: CSize, data: Ptr[Byte]): CSize = {
-      val serial = !(data.asInstanceOf[Ptr[Long]])
-      val len = stackalloc[Double]
-      !len = 0
-      val byteSize = size * nmemb
-      val headerString = bufferToString(ptr, size, nmemb)
-      headerString match {
-        case statusLine(code, description) =>
-          println(s"status code: $code $description")
-        case headerLine(k, v) =>
-          val resp = responses(serial)
-          resp.headers(k) = v
-          responses(serial) = resp
-        case l =>
-      }
-      fwrite(ptr, size, nmemb, stdout)
-      return byteSize
-    }
-  }
+  println("initializing")
+  global_init(1)
+  val resp = getSync(url)
+  println(s"done.  got response: $resp")
+  println("global cleanup...")
+  global_cleanup()
+  println("done")
